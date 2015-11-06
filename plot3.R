4995c3d9c0fc37b51b3c7841c521bfc38a4cbdbe
lirinka <lirinka@gmail.com>
2015-11-06 15:12:32 +0800

    fixed na values character

--- plot3.R
+++ plot3.R
@@ -1,7 +1,7 @@
 ### Plot 3: energy submetering
 ## read data
 #read the first row to create a header
-header <- read.table("household_power_consumption.txt", nrow = 1, header = FALSE, sep =';', stringsAsFactors = F)
+header <- read.table("household_power_consumption.txt", nrow = 1, header = FALSE, sep =';', stringsAsFactors = F, na.strings="?")
 #read the rows of data we need
 dat <- read.table("household_power_consumption.txt", skip=66636, nrow=2880, header=TRUE, sep=";", dec=".", stringsAsFactors=F)
 #add the header
